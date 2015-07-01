4f1f9e9eca820836a6adba7fada126dd3408019f
Johan Hovold <johan@hovoldconsulting.com>
2015-07-01 12:37:26 +0200

    greybus: operation: add incoming-operation flag
    Add flag field to struct gb_operation, and a first flag
    GB_OPERATION_FLAG_INCOMING to identify incoming operations.
    
    Pass an initial set of flags when allocating new operations, and use
    these to identify incoming operations rather than overloading the
    meaning of GB_OPERATION_TYPE_INVALID. This also allows us to set the
    type for all operations during allocation.
    
    Also add convenience helper to identify incoming operations.
    
    Signed-off-by: Johan Hovold <johan@hovoldconsulting.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/staging/greybus/operation.h
+++ drivers/staging/greybus/operation.h
@@ -88,6 +88,8 @@ struct gb_message {
 	void				*buffer;
 };
 
+#define GB_OPERATION_FLAG_INCOMING		BIT(0)
+
 /*
  * A Greybus operation is a remote procedure call performed over a
  * connection between two UniPro interfaces.
@@ -112,8 +114,9 @@ struct gb_operation {
 	struct gb_connection	*connection;
 	struct gb_message	*request;
 	struct gb_message	*response;
-	u8			type;
 
+	unsigned long		flags;
+	u8			type;
 	u16			id;
 	int			errno;		/* Operation result */
 
@@ -124,6 +127,12 @@ struct gb_operation {
 	struct kref		kref;
 	struct list_head	links;		/* connection->operations */
 };
+
+static inline bool
+gb_operation_is_incoming(struct gb_operation *operation)
+{
+	return operation->flags & GB_OPERATION_FLAG_INCOMING;
+}
 
 void gb_connection_recv(struct gb_connection *connection,
 					void *data, size_t size);
