91784fddf669b2f41c4489a03cc093a2062f3f24
Rasmus Villemoes <linux@rasmusvillemoes.dk>
2015-10-29 12:01:41 +0100

    fs/file.c: __const_max is actually __const_min :-)
    7f4b36f9bb930 "get rid of files_defer_init()" inexplicably changed a
    min() to a __const_max() - but the __const_max macro actually gives
    the minimum... So no functional change, just less confusing naming.
    
    Signed-off-by: Rasmus Villemoes <linux@rasmusvillemoes.dk>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/file.c
+++ fs/file.c
@@ -30,9 +30,9 @@ extern void check_fd_invalid_close(int fd, struct task_struct *cur, struct files
 
 int sysctl_nr_open __read_mostly = 1024*1024;
 int sysctl_nr_open_min = BITS_PER_LONG;
-/* our max() is unusable in constant expressions ;-/ */
-#define __const_max(x, y) ((x) < (y) ? (x) : (y))
-int sysctl_nr_open_max = __const_max(INT_MAX, ~(size_t)0/sizeof(void *)) &
+/* our min() is unusable in constant expressions ;-/ */
+#define __const_min(x, y) ((x) < (y) ? (x) : (y))
+int sysctl_nr_open_max = __const_min(INT_MAX, ~(size_t)0/sizeof(void *)) &
 			 -BITS_PER_LONG;
 
 #ifdef CONFIG_SEC_DEBUG_FILE_LEAK
