2c77ae00dcede47bbd8a9281fd91ffca238af79a
Yaowei Bai <baiyaowei@cmss.chinamobile.com>
2015-11-17 14:40:10 +0800

    fs/namespace.c: path_is_under can be boolean
    This patch makes path_is_under return bool to improve
    readability due to this particular function only using either
    one or zero as its return value.
    
    No functional change.
    
    Signed-off-by: Yaowei Bai <baiyaowei@cmss.chinamobile.com>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/namespace.c
+++ fs/namespace.c
@@ -3663,9 +3663,9 @@ bool is_path_reachable(struct mount *mnt, struct dentry *dentry,
 #endif
 }
 
-int path_is_under(struct path *path1, struct path *path2)
+bool path_is_under(struct path *path1, struct path *path2)
 {
-	int res;
+	bool res;
 	read_seqlock_excl(&mount_lock);
 	res = is_path_reachable(real_mount(path1->mnt), path1->dentry, path2);
 	read_sequnlock_excl(&mount_lock);
