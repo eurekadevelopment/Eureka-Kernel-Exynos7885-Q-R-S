6785ffef3ea6ebaa3e019c585ee674fb11fdf46e
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2015-12-07 13:09:52 -0800

    rcu: Remove TINY_RCU bloat from pointless boot parameters
    The rcu_expedited, rcu_normal, and rcu_normal_after_boot kernel boot
    parameters are pointless in the case of TINY_RCU because in that case
    synchronous grace periods, both expedited and normal, are no-ops.
    However, these three symbols contribute several hundred bytes of bloat.
    This commit therefore uses CPP directives to avoid compiling this code
    in TINY_RCU kernels.
    
    Reported-by: kbuild test robot <fengguang.wu@intel.com>
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Reviewed-by: Josh Triplett <josh@joshtriplett.org>
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>
    Signed-off-by: Santhosh <san8754513543@gmail.com>

--- kernel/ksysfs.c
+++ kernel/ksysfs.c
@@ -153,6 +153,7 @@ static ssize_t fscaps_show(struct kobject *kobj,
 }
 KERNEL_ATTR_RO(fscaps);
 
+#ifndef CONFIG_TINY_RCU
 int rcu_expedited;
 static ssize_t rcu_expedited_show(struct kobject *kobj,
 				  struct kobj_attribute *attr, char *buf)
@@ -186,6 +187,7 @@ static ssize_t rcu_normal_store(struct kobject *kobj,
 	return count;
 }
 KERNEL_ATTR_RW(rcu_normal);
+#endif /* #ifndef CONFIG_TINY_RCU */
 
 /*
  * Make /sys/kernel/notes give the raw contents of our kernel .notes section.
@@ -228,10 +230,9 @@ static struct attribute * kernel_attrs[] = {
 	&kexec_crash_size_attr.attr,
 	&vmcoreinfo_attr.attr,
 #endif
+#ifndef CONFIG_TINY_RCU
 	&rcu_expedited_attr.attr,
 	&rcu_normal_attr.attr,
-#ifdef CONFIG_PREEMPT_RT_FULL
-	&realtime_attr.attr,
 #endif
 	NULL
 };
