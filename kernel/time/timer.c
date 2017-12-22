d25e9dc1f070b155a0a30f5a27669b574d5708d1
Thomas Gleixner <tglx@linutronix.de>
2017-12-22 15:51:14 +0100

    timer: Invoke timer_start_debug() where it makes sense
    The timer start debug function is called before the proper timer base is
    set. As a consequence the trace data contains the stale CPU and flags
    values.
    
    Call the debug function after setting the new base and flags.
    
    Fixes: 500462a9de65 ("timers: Switch to a non-cascading wheel")
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Cc: stable@vger.kernel.org
    Cc: rt@linutronix.de
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Signed-off-by: Daniel Wagner <wagi@monom.org>

--- kernel/time/timer.c
+++ kernel/time/timer.c
@@ -839,8 +839,6 @@ __mod_timer(struct timer_list *timer, unsigned long expires,
 	if (!ret && pending_only)
 		goto out_unlock;
 
-	debug_activate(timer, expires);
-
 	new_base = get_target_base(base, pinned);
 
 	if (base != new_base) {
@@ -854,6 +852,8 @@ __mod_timer(struct timer_list *timer, unsigned long expires,
 		if (likely(base->running_timer != timer))
 			base = switch_timer_base(timer, base, new_base);
 	}
+
+	debug_activate(timer, expires);
 
 	timer->expires = expires;
 	internal_add_timer(base, timer);
