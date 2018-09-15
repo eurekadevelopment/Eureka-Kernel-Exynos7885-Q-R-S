e72e637406318f057832e00f415ecb741a0bd374
celtare21 <celtare21@gmail.com>
2018-09-15 20:09:09 +0000

    tcp_westwood: Make the RTT values static and not tunable
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Signed-off-by: Atman Shah <masteratman@gmail.com>
    Signed-off-by: sohamxda7 <sensoham135@gmail.com>
    Signed-off-by: Prashant-1695 <prashant33968@gmail.com>
    Signed-off-by: ImPrashantt <prashant33968@gmail.com>

--- net/ipv4/tcp_westwood.c
+++ net/ipv4/tcp_westwood.c
@@ -43,12 +43,8 @@ struct westwood {
 };
 
 /* TCP Westwood functions and constants */
-const int tcp_westwood_rtt_min = 37;
-const int tcp_westwood_init_rtt = 1800;
-
-/* Let's make them tunable */
-module_param_named(rtt_min, tcp_westwood_rtt_min, int, 0644);
-module_param_named(rtt_init, tcp_westwood_init_rtt, int, 0644);
+static const int tcp_westwood_rtt_min = 37;
+static const int tcp_westwood_init_rtt = 1800;
 
 /*
  * @tcp_westwood_create
