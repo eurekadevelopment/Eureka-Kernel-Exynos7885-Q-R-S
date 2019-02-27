90acabb58609eed97e3bfdbf5a1ccc2397211004
Rafael J. Wysocki <rafael.j.wysocki@intel.com>
2019-02-27 14:35:50 +0100

    cpuidle: menu: Avoid overflows when computing variance
    The variance computation in get_typical_interval() may overflow if
    the square of the value of diff exceeds the maximum for the int64_t
    data type value which basically is the case when it is of the order
    of UINT_MAX.
    
    However, data points so far in the future don't matter for idle
    state selection anyway, so change the initial threshold value in
    get_typical_interval() to INT_MAX which will cause more "outlying"
    data points to be discarded without affecting the selection result.
    
    Reported-by: Randy Dunlap <rdunlap@infradead.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>

--- drivers/cpuidle/governors/menu.c
+++ drivers/cpuidle/governors/menu.c
@@ -188,7 +188,7 @@ static void get_typical_interval(struct menu_device *data)
 	unsigned int max, thresh;
 	uint64_t avg, variance;
 
-	thresh = UINT_MAX; /* Discard outliers above this value */
+	thresh = INT_MAX; /* Discard outliers above this value */
 
 again:
 
