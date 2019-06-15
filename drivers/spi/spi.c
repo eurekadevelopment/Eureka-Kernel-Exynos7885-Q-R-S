5175768546c919155981dcb7402ec1db0c5b7bfb
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2019-06-15 20:41:35 +0300

    spi: No need to assign dummy value in spi_unregister_controller()
    [ Upstream commit ebc37af5e0a134355ea2b62ed4141458bdbd5389 ]
    
    The device_for_each_child() doesn't require the returned value to be checked.
    Thus, drop the dummy variable completely and have no warning anymore:
    
    drivers/spi/spi.c: In function ‘spi_unregister_controller’:
    drivers/spi/spi.c:2480:6: warning: variable ‘dummy’ set but not used [-Wunused-but-set-variable]
      int dummy;
          ^~~~~
    
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/spi/spi.c
+++ drivers/spi/spi.c
@@ -1923,8 +1923,6 @@ static int __unregister(struct device *dev, void *null)
  */
 void spi_unregister_master(struct spi_master *master)
 {
-	int dummy;
-
 	if (master->queued) {
 		if (spi_destroy_queue(master))
 			dev_err(&master->dev, "queue remove failed\n");
@@ -1934,7 +1932,7 @@ void spi_unregister_master(struct spi_master *master)
 	list_del(&master->list);
 	mutex_unlock(&board_lock);
 
-	dummy = device_for_each_child(&master->dev, NULL, __unregister);
+	device_for_each_child(&master->dev, NULL, __unregister);
 	device_unregister(&master->dev);
 }
 EXPORT_SYMBOL_GPL(spi_unregister_master);
