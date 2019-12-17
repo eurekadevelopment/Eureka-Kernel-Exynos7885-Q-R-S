7950546ad48fb690134cc439fbefc5f5c59e0e2f
Jaihind Yadav <jaihindyadav@codeaurora.org>
2019-12-17 17:25:47 +0530

    selinux: ensure we cleanup the internal AVC counters on error in avc_update()
    [ Upstream commit 030b995ad9ece9fa2d218af4429c1c78c2342096 ]
    
    In AVC update we don't call avc_node_kill() when avc_xperms_populate()
    fails, resulting in the avc->avc_cache.active_nodes counter having a
    false value.  In last patch this changes was missed , so correcting it.
    
    Fixes: fa1aa143ac4a ("selinux: extended permissions for ioctls")
    Signed-off-by: Jaihind Yadav <jaihindyadav@codeaurora.org>
    Signed-off-by: Ravi Kumar Siddojigari <rsiddoji@codeaurora.org>
    [PM: merge fuzz, minor description cleanup]
    Signed-off-by: Paul Moore <paul@paul-moore.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- security/selinux/avc.c
+++ security/selinux/avc.c
@@ -874,11 +874,7 @@ static int avc_update_node(u32 event, u32 perms, u8 driver, u8 xperm, u32 ssid,
 	if (orig->ae.xp_node) {
 		rc = avc_xperms_populate(node, orig->ae.xp_node);
 		if (rc) {
-//[SEC_SELINUX_PORTING_COMMON
-// P191014-03912 - avc_cache.active_nodes is not decresed when "avc_alloc_node-success"&"avc_xperms_populate-fail"
-//			kmem_cache_free(avc_node_cachep, node);
 			avc_node_kill(node);
-//]SEC_SELINUX_PORTING_COMMON
 			goto out_unlock;
 		}
 	}
