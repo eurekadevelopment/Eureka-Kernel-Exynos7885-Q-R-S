09a48d94d443da522234de64b2c6f8f72ba24650
Jan Kara <jack@suse.cz>
2020-06-10 17:36:03 +0200

    writeback: Protect inode->i_io_list with inode->i_lock
    commit b35250c0816c7cf7d0a8de92f5fafb6a7508a708 upstream.
    
    Currently, operations on inode->i_io_list are protected by
    wb->list_lock. In the following patches we'll need to maintain
    consistency between inode->i_state and inode->i_io_list so change the
    code so that inode->i_lock protects also all inode's i_io_list handling.
    
    Reviewed-by: Martijn Coenen <maco@android.com>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    CC: stable@vger.kernel.org # Prerequisite for "writeback: Avoid skipping inode writeback"
    Signed-off-by: Jan Kara <jack@suse.cz>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- fs/fs-writeback.c
+++ fs/fs-writeback.c
@@ -1051,7 +1051,8 @@ void inode_io_list_del(struct inode *inode)
  * the case then the inode must have been redirtied while it was being written
  * out and we don't reset its dirtied_when.
  */
-static void __redirty_tail(struct inode *inode, struct bdi_writeback *wb)
+
+static void __redirty_tail_locked(struct inode *inode, struct bdi_writeback *wb)
 {
 	assert_spin_locked(&inode->i_lock);
 	if (!list_empty(&wb->b_dirty)) {
@@ -1068,7 +1069,7 @@ static void __redirty_tail(struct inode *inode, struct bdi_writeback *wb)
 static void redirty_tail(struct inode *inode, struct bdi_writeback *wb)
 {
 	spin_lock(&inode->i_lock);
-	__redirty_tail(inode, wb);
+	__redirty_tail_locked(inode, wb);
 	spin_unlock(&inode->i_lock);
 }
 
@@ -1285,7 +1286,7 @@ static void requeue_inode(struct inode *inode, struct bdi_writeback *wb,
 		 * writeback is not making progress due to locked
 		 * buffers. Skip this inode for now.
 		 */
-		__redirty_tail(inode, wb);
+		__redirty_tail_locked(inode, wb);
 		return;
 	}
 
@@ -1305,7 +1306,8 @@ static void requeue_inode(struct inode *inode, struct bdi_writeback *wb,
 			 * retrying writeback of the dirty page/inode
 			 * that cannot be performed immediately.
 			 */
-			__redirty_tail(inode, wb);
+			__redirty_tail_locked(inode, wb);
+
 		}
 	} else if (inode->i_state & I_DIRTY) {
 		/*
@@ -1313,7 +1315,7 @@ static void requeue_inode(struct inode *inode, struct bdi_writeback *wb,
 		 * such as delayed allocation during submission or metadata
 		 * updates after data IO completion.
 		 */
-		__redirty_tail(inode, wb);
+		__redirty_tail_locked(inode, wb);
 	} else if (inode->i_state & I_DIRTY_TIME) {
 		inode->dirtied_when = jiffies;
 		inode_io_list_move_locked(inode, wb, &wb->b_dirty_time);
@@ -1565,7 +1567,7 @@ static long writeback_sb_inodes(struct super_block *sb,
 		spin_lock(&inode->i_lock);
 		if (inode->i_state & (I_NEW | I_FREEING | I_WILL_FREE)) {
 			inode->i_state &= ~I_SYNC_QUEUED;
-			__redirty_tail(inode, wb);
+			__redirty_tail_locked(inode, wb);
 			spin_unlock(&inode->i_lock);
 			continue;
 		}
