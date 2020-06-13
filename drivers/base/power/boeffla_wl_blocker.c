05fbe50efd10a86a5dcace6f2935aba638f116ff
Denis Efremov <efremov@linux.com>
2020-06-13 16:16:07 +0300

    power: wl_blocker: remove excessive len variable
    It's not used. Type conversion size_t > int is not
    needed.
    
    Signed-off-by: Denis Efremov <efremov@linux.com>

--- drivers/base/power/boeffla_wl_blocker.c
+++ drivers/base/power/boeffla_wl_blocker.c
@@ -82,10 +82,8 @@ static ssize_t wakelock_blocker_show(struct device *dev, struct device_attribute
 static ssize_t wakelock_blocker_store(struct device * dev, struct device_attribute *attr,
 			     const char * buf, size_t n)
 {
-	int len = n;
-
 	// check if string is too long to be stored
-	if (len > LENGTH_LIST_WL)
+	if (n > LENGTH_LIST_WL)
 		return -EINVAL;
 
 	// store user configured wakelock list and rebuild search string
@@ -109,10 +107,8 @@ static ssize_t wakelock_blocker_default_show(struct device *dev, struct device_a
 static ssize_t wakelock_blocker_default_store(struct device * dev, struct device_attribute *attr,
 			     const char * buf, size_t n)
 {
-	int len = n;
-
 	// check if string is too long to be stored
-	if (len > LENGTH_LIST_WL_DEFAULT)
+	if (n > LENGTH_LIST_WL_DEFAULT)
 		return -EINVAL;
 
 	// store default, predefined wakelock list and rebuild search string
