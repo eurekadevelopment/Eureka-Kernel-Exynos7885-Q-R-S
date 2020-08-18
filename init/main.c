f31e3e26ea7ea3c83218f93c1a6b2d70cfd3144d
Oscar Salvador <osalvador@suse.de>
2020-08-18 13:00:46 +0200

    mm: Avoid calling build_all_zonelists_init under hotplug context
    Recently a customer of ours experienced a crash when booting the
    system while enabling memory-hotplug.
    
    The problem is that Normal zones on different nodes don't get their private
    zone->pageset allocated, and keep sharing the initial boot_pageset.
    The sharing between zones is normally safe as explained by the comment for
    boot_pageset - it's a percpu structure, and manipulations are done with
    disabled interrupts, and boot_pageset is set up in a way that any page placed
    on its pcplist is immediately flushed to shared zone's freelist, because
    pcp->high == 1.
    However, the hotplug operation updates pcp->high to a higher value as it
    expects to be operating on a private pageset.
    
    The problem is in build_all_zonelists(), which is called when the first range
    of pages is onlined for the Normal zone of node X or Y:
    
    	if (system_state == SYSTEM_BOOTING) {
    		build_all_zonelists_init();
    	} else {
    	#ifdef CONFIG_MEMORY_HOTPLUG
    		if (zone)
    			setup_zone_pageset(zone);
    	#endif
    		/* we have to stop all cpus to guarantee there is no user
    		of zonelist */
    		stop_machine(__build_all_zonelists, pgdat, NULL);
    		/* cpuset refresh routine should be here */
    	}
    
    When called during hotplug, it should execute the setup_zone_pageset(zone)
    which allocates the private pageset.
    However, with memhp_default_state=online, this happens early while
    system_state == SYSTEM_BOOTING is still true, hence this step is skipped.
    (and build_all_zonelists_init() is probably unsafe anyway at this point).
    
    Another hotplug operation on the same zone then leads to zone_pcp_update(zone)
    called from online_pages(), which updates the pcp->high for the shared
    boot_pageset to a value higher than 1.
    At that point, pages freed from Node X and Y Normal zones can end up on the same
    pcplist and from there they can be freed to the wrong zone's freelist,
    leading to the corruption and crashes.
    
    Please, note that upstream has fixed that differently (and unintentionally) by
    adding another boot state (SYSTEM_SCHEDULING), which is set before smp_init().
    That should happen before memory hotplug events even with memhp_default_state=online.
    Backporting that would be too intrusive.
    
    Signed-off-by: Oscar Salvador <osalvador@suse.de>
    Debugged-by: Vlastimil Babka <vbabka@suse.cz>
    Acked-by: Michal Hocko <mhocko@suse.com> # for stable trees
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- init/main.c
+++ init/main.c
@@ -704,7 +704,7 @@ asmlinkage __visible void __init start_kernel(void)
 	setup_per_cpu_areas();
 	smp_prepare_boot_cpu();	/* arch-specific boot-cpu hooks */
 
-	build_all_zonelists(NULL, NULL);
+	build_all_zonelists(NULL, NULL, false);
 	page_alloc_init();
 
 #if !defined(CONFIG_SAMSUNG_PRODUCT_SHIP)
