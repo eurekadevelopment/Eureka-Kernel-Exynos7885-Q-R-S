f4d6c21e18ff71de2b2257eda3db131ad94ea974
Chatur27 <jasonbright2709@gmail.com>
2020-08-20 20:46:38 +0400

    staging: ion: Fix errors caused by disabling ION_EXYNOS_STAT_LOG
    Change-Id: I955dae9f71490f12d6d657756385a91a5fda5761
    Signed-off-by: Chatur27 <jasonbright2709@gmail.com>

--- drivers/staging/android/ion/ion.c
+++ drivers/staging/android/ion/ion.c
@@ -1033,10 +1033,17 @@ static int ion_debug_client_show(struct seq_file *s, void *unused)
 		return -EINVAL;
 	}
 
+#ifdef CONFIG_ION_EXYNOS_STAT_LOG
 	seq_printf(s, "%16.s %4.s %16.s %4.s %10.s %8.s %9.s\n",
 		   "task", "pid", "thread", "tid", "size", "# procs", "flag");
 	seq_printf(s, "----------------------------------------------"
 			"--------------------------------------------\n");
+#else
+	seq_printf(s, "%16.s %4.s %10.s %8.s %9.s\n",
+		   "task", "pid", "size", "# procs", "flag");
+	seq_printf(s, "----------------------------------------------"
+			"--------------------------------------------\n");
+#endif
 
 	mutex_lock(&client->lock);
 	for (n = rb_first(&client->handles); n; n = rb_next(n)) {
@@ -1049,10 +1056,16 @@ static int ion_debug_client_show(struct seq_file *s, void *unused)
 			names[id] = buffer->heap->name;
 		sizes[id] += buffer->size;
 		sizes_pss[id] += (buffer->size / buffer->handle_count);
+#ifdef CONFIG_ION_EXYNOS_STAT_LOG
 		seq_printf(s, "%16.s %4u %16.s %4u %10zu %8d %9lx\n",
 			   buffer->task_comm, buffer->pid,
 				buffer->thread_comm, buffer->tid, buffer->size,
 				buffer->handle_count, buffer->flags);
+#else
+		seq_printf(s, "%16.s %4u %10zu %8d %9lx\n",
+			   buffer->task_comm, buffer->pid,
+				buffer->size, buffer->handle_count, buffer->flags);
+#endif
 	}
 	mutex_unlock(&client->lock);
 	up_read(&g_idev->lock);
