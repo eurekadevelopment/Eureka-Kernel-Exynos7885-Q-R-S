2709fed10d56c653da78453d5170da25e2610ddc
andip71 <andreasp@gmx.de>
2020-08-21 19:43:50 +0400

    boeffla_wl_blocker: add generic wakelock blocker driver v1.1.0
    Based on ideas of FranciscoFranco's non-generic driver.
    
    *Added some wakelocks to the blacklist
    
    Signed-off-by: andip71 <andreasp@gmx.de>
    Signed-off-by: engstk <eng.stk@sapo.pt>
    Signed-off-by: Denis Efremov <efremov@linux.com>
    Signed-off-by: Chatur27 <jasonbright2709@gmail.com>

---
+++ drivers/base/power/boeffla_wl_blocker.h
@@ -0,0 +1,23 @@
+/*
+ * Author: andip71, 01.09.2017
+ *
+ * Version 1.1.0
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#define BOEFFLA_WL_BLOCKER_VERSION	"1.1.0"
+
+#define LIST_WL_DEFAULT			"wlan_wake;wlan_rx_wake;wlan_ctrl_wake;wlan_txfl_wake;bluetooth_timer;BT_bt_wake;BT_host_wake;bbd_wake_lock;ssp_sensorhub_wake_lock;14860000.decon_f;ssp_wake_lock;ssp_comm_wake_lock;abox;umts_ipc0;umts_ipc1;mmc0_detect"
+
+#define LENGTH_LIST_WL			2048
+#define LENGTH_LIST_WL_DEFAULT		255
+#define LENGTH_LIST_WL_SEARCH		LENGTH_LIST_WL + LENGTH_LIST_WL_DEFAULT + 5
