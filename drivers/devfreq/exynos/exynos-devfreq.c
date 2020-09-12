a6b63baa314eccab77a9abeb68fcb8a0766e8b59
Chatur27 <jasonbright2709@gmail.com>
2020-09-12 19:43:43 +0400

    defconfig: Enable devfreq_boost driver
    * Also, fix code in exynos-devfreq.c
    
    Change-Id: I12150c60ec8670bed93216a6509248cdc750735d
    Signed-off-by: Chatur27 <jasonbright2709@gmail.com>

--- drivers/devfreq/exynos/exynos-devfreq.c
+++ drivers/devfreq/exynos/exynos-devfreq.c
@@ -27,6 +27,7 @@
 #include <linux/io.h>
 #include <linux/sched.h>
 #include <linux/exynos-wd.h>
+#include <linux/devfreq_boost.h>
 
 #include <soc/samsung/exynos-devfreq.h>
 #include <soc/samsung/tmu.h>
@@ -2174,6 +2175,9 @@ static int exynos_devfreq_probe(struct platform_device *pdev)
 		goto err_devfreq;
 	}
 
+	if (data->devfreq_type == DEVFREQ_MIF)
+		devfreq_register_boost_device(DEVFREQ_EXYNOS_MIF, data->devfreq);
+	
 	data->devfreq->min_freq = data->min_freq;
 	data->devfreq->max_freq = data->max_freq;
 
