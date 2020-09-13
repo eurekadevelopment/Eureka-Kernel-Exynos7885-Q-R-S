03b919291338e63b1cd5b5d73cf45fe82d72e037
raymanfx <raymanfx@gmail.com>
2020-09-13 19:00:56 +0400

    video: mdnie: Lift RGB tuning restrictions
    Change-Id: Iad5d7dea8883bb5a21f13e810bafac4ba211f66b
    Signed-off-by: Chatur27 <jasonbright2709@gmail.com>

--- drivers/video/fbdev/exynos/dpu_7885/panels/mdnie_lite.c
+++ drivers/video/fbdev/exynos/dpu_7885/panels/mdnie_lite.c
@@ -561,11 +561,7 @@ static ssize_t sensorRGB_store(struct device *dev,
 	if (ret < 0)
 		return ret;
 
-	if (mdnie->enable
-		&& mdnie->accessibility == ACCESSIBILITY_OFF
-		&& !mdnie->ldu
-		&& mdnie->mode == AUTO
-		&& (mdnie->scenario == BROWSER_MODE || mdnie->scenario == EBOOK_MODE)) {
+	if (mdnie->enable) {
 		dev_info(dev, "%s: %d, %d, %d\n", __func__, white_r, white_g, white_b);
 
 		table = mdnie_find_table(mdnie);
