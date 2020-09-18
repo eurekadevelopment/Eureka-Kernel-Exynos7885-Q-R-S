9badb199698ee4b569d339f4672284dbfd599ce8
Al Viro <viro@zeniv.linux.org.uk>
2020-09-18 16:45:50 -0400

    [PATCH] reduce boilerplate in fsid handling
    Get rid of boilerplate in most of ->statfs()
    instances...
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

--- fs/ext4/super.c
+++ fs/ext4/super.c
@@ -5181,8 +5181,7 @@ static int ext4_statfs(struct dentry *dentry, struct kstatfs *buf)
 	buf->f_namelen = EXT4_NAME_LEN;
 	fsid = le64_to_cpup((void *)es->s_uuid) ^
 	       le64_to_cpup((void *)es->s_uuid + sizeof(u64));
-	buf->f_fsid.val[0] = fsid & 0xFFFFFFFFUL;
-	buf->f_fsid.val[1] = (fsid >> 32) & 0xFFFFFFFFUL;
+	buf->f_fsid = u64_to_fsid(fsid);
 
 	return 0;
 }
