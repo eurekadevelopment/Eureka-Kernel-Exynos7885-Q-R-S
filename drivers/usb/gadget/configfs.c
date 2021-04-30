8004404a02115ad10ab312a1aa8914b80a882043
chatur27 <jasonbright2709@gmail.com>
2021-04-30 12:55:37 +0400

    drivers/usb/gadget: Fix [-Wunused-function] warning
    ../drivers/usb/gadget/configfs.c:1739:12: warning: unused function 'configfs_composite_setup' [-Wunused-function]
    static int configfs_composite_setup(struct usb_gadget *gadget,
               ^
    ../drivers/usb/gadget/configfs.c:1764:13: warning: unused function 'configfs_composite_disconnect' [-Wunused-function]
    static void configfs_composite_disconnect(struct usb_gadget *gadget)
                ^
    
    Change-Id: Icf29e07ea4c75f561e2b079e0ec8ac9b76f7a561
    Signed-off-by: Chatur27 <jasonbright2709@gmail.com>

--- drivers/usb/gadget/configfs.c
+++ drivers/usb/gadget/configfs.c
@@ -1810,6 +1810,7 @@ static void configfs_composite_unbind(struct usb_gadget *gadget)
 	spin_unlock_irqrestore(&gi->spinlock, flags);
 }
 
+/*
 static int configfs_composite_setup(struct usb_gadget *gadget,
 		const struct usb_ctrlrequest *ctrl)
 {
@@ -1834,7 +1835,9 @@ static int configfs_composite_setup(struct usb_gadget *gadget,
 	spin_unlock_irqrestore(&gi->spinlock, flags);
 	return ret;
 }
+*/
 
+/*
 static void configfs_composite_disconnect(struct usb_gadget *gadget)
 {
 	struct usb_composite_dev *cdev;
@@ -1856,6 +1859,7 @@ static void configfs_composite_disconnect(struct usb_gadget *gadget)
 	composite_disconnect(gadget);
 	spin_unlock_irqrestore(&gi->spinlock, flags);
 }
+*/
 
 static void configfs_composite_suspend(struct usb_gadget *gadget)
 {
