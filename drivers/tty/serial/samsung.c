79385f135e6acd16524b7a007e096bc1d3d7654c
chatur27 <jasonbright2709@gmail.com>
2021-04-30 12:24:11 +0400

    drivers/tty/serial: Fix [-Wunused-variable] warning
    ../drivers/tty/serial/samsung.c:999:7: warning: unused variable 'clkname' [-Wunused-variable]
            char clkname[MAX_CLK_NAME_LENGTH];
                 ^
    
    Change-Id: I0aa6464661406021e348863ebeb720b0d5dda15a
    Signed-off-by: Chatur27 <jasonbright2709@gmail.com>

--- drivers/tty/serial/samsung.c
+++ drivers/tty/serial/samsung.c
@@ -996,7 +996,7 @@ static unsigned int s3c24xx_serial_getclk(struct s3c24xx_uart_port *ourport,
 	struct s3c24xx_uart_info *info = ourport->info;
 	unsigned long rate;
 	unsigned int cnt, baud, quot, best_quot = 0;
-	char clkname[MAX_CLK_NAME_LENGTH];
+	/* char clkname[MAX_CLK_NAME_LENGTH]; */
 	int calc_deviation, deviation = (1 << 30) - 1;
 
 	for (cnt = 0; cnt < info->num_clks; cnt++) {
