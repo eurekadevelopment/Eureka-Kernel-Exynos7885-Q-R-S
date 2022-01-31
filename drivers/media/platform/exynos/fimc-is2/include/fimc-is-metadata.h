336e4512517bf4d5b6470088167c629adae12695
Chatur27 <jasonbright2709@gmail.com>
2022-01-31 19:56:59 +0000

    drivers/media/platform/exynos/fimc-is2: Merge remaining changes needed for OneUI3
    * However, make it optional.
    
    Change-Id: I9baab347cd0282e60b884e1fdf9b67f318f90069
    Signed-off-by: Chatur27 <jasonbright2709@gmail.com>

--- drivers/media/platform/exynos/fimc-is2/include/fimc-is-metadata.h
+++ drivers/media/platform/exynos/fimc-is2/include/fimc-is-metadata.h
@@ -2165,6 +2165,16 @@ struct hfd_meta {
 	uint32_t		hw_rot_mirror[CAMERA2_MAX_FACES];
 };
 
+#ifdef CONFIG_ONEUI3_FIMC_IS2_CAMERA_HEADER
+enum camera_flip_mode {
+	CAM_FLIP_MODE_NORMAL = 0,
+	CAM_FLIP_MODE_HORIZONTAL,
+	CAM_FLIP_MODE_VERTICAL,
+	CAM_FLIP_MODE_HORIZONTAL_VERTICAL,
+	CAM_FLIP_MODE_MAX,
+};
+#endif
+
 /** \brief
   stream structure for scaler.
  */
@@ -2314,6 +2324,11 @@ struct camera2_shot_ext {
 	uint32_t			bds_ratio_x;
 	uint32_t			bds_ratio_y;
 	uint32_t			remosaic_rotation;
+
+#ifdef CONFIG_ONEUI3_FIMC_IS2_CAMERA_HEADER
+	enum camera_flip_mode		mcsc_flip[MCSC_PORT_MAX];
+	enum camera_flip_mode		mcsc_flip_result[MCSC_PORT_MAX];
+#endif
 
 	/* reserved for future */
 	uint32_t			reserved[7];
