98ebe41651c8dbfaa71b4292c180b03b205b956a
Soo-Hwan Na <69458352+roynatech2544@users.noreply.github.com>
2022-04-04 20:18:38 +0900

    fs: proc: Reset warrenty bit to 0

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -47,9 +47,10 @@ static void remove_safetynet_flags(char *cmd)
 
 static int __init proc_cmdline_init(void)
 {
-	/* SafetyNet bypass: show androidboot.verifiedbootstate=green */
 	char *a1, *a2;
-
+	bool is_detected = false;
+	
+	/* SafetyNet bypass: show androidboot.verifiedbootstate=green */
 	a1 = strstr(saved_command_line, "androidboot.verifiedbootstate=");
 	if (a1) {
 		a1 = strchr(a1, '=');
@@ -60,7 +61,24 @@ static int __init proc_cmdline_init(void)
 		scnprintf(proc_cmdline, COMMAND_LINE_SIZE, "%.*sgreen%s",
 			  (int)(a1 - saved_command_line + 1),
 			  saved_command_line, a2);
-	} else {
+		is_detected = true;
+	}
+	
+	/* TEE check bypass: show androidboot.warrenty_bit=0 */
+	a1 = strstr(saved_command_line, "androidboot.warranty_bit=");
+	if (a1) {
+		a1 = strchr(a1, '=');
+		a2 = strchr(a1, ' ');
+		if (!a2) /* last argument on the cmdline */
+			a2 = "";
+
+		scnprintf(proc_cmdline, COMMAND_LINE_SIZE, "%.*s0%s",
+			  (int)(a1 - saved_command_line + 1),
+			  saved_command_line, a2);
+		is_detected = true;
+	}
+	
+	if (!is_detected) {
 		strncpy(proc_cmdline, saved_command_line, COMMAND_LINE_SIZE);
 	}
 	
