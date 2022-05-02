3839e44e83ec6405a36f0b459cdf01a27920ace2
Willy Tarreau <w@1wt.eu>
2022-05-02 10:46:12 +0200

    tcp: dynamically allocate the perturb table used by source ports
    commit e9261476184be1abd486c9434164b2acbe0ed6c2 upstream.
    
    We'll need to further increase the size of this table and it's likely
    that at some point its size will not be suitable anymore for a static
    table. Let's allocate it on boot from inet_hashinfo2_init(), which is
    called from tcp_init().
    
    Cc: Moshe Kol <moshe.kol@mail.huji.ac.il>
    Cc: Yossi Gilad <yossi.gilad@mail.huji.ac.il>
    Cc: Amit Klein <aksecurity@gmail.com>
    Reviewed-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: Willy Tarreau <w@1wt.eu>
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    [bwh: Backported to 4.9:
     - There is no inet_hashinfo2_init(), so allocate the table in
       inet_hashinfo_init() when called by TCP
     - Adjust context]
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    [pavel: backport to 4.4, switch to simpler method of doing allocation once]
    Signed-off-by: Pavel Machek <pavel@denx.de>

--- net/ipv4/inet_hashtables.c
+++ net/ipv4/inet_hashtables.c
@@ -568,7 +568,8 @@ EXPORT_SYMBOL_GPL(inet_unhash);
  * privacy, this only consumes 1 KB of kernel memory.
  */
 #define INET_TABLE_PERTURB_SHIFT 8
-static u32 table_perturb[1 << INET_TABLE_PERTURB_SHIFT];
+#define INET_TABLE_PERTURB_SIZE (1 << INET_TABLE_PERTURB_SHIFT)
+static u32 *table_perturb;
 
 int __inet_hash_connect(struct inet_timewait_death_row *death_row,
 		struct sock *sk, u64 port_offset,
@@ -588,7 +589,8 @@ int __inet_hash_connect(struct inet_timewait_death_row *death_row,
 		u32 offset;
 		struct inet_timewait_sock *tw = NULL;
 
-		net_get_random_once(table_perturb, sizeof(table_perturb));
+		net_get_random_once(table_perturb,
+				    INET_TABLE_PERTURB_SIZE * sizeof(*table_perturb));
 		index = hash_32(port_offset, INET_TABLE_PERTURB_SHIFT);
 		offset = READ_ONCE(table_perturb[index]) + (port_offset >> 32);
 
@@ -719,7 +721,16 @@ void inet_hashinfo_init(struct inet_hashinfo *h)
 		spin_lock_init(&h->listening_hash[i].lock);
 		INIT_HLIST_NULLS_HEAD(&h->listening_hash[i].head,
 				      i + LISTENING_NULLS_BASE);
-		}
+	}
+
+	if (table_perturb)
+		return;
+
+	/* this one is used for source ports of outgoing connections */
+	table_perturb = kmalloc_array(INET_TABLE_PERTURB_SIZE,
+				      sizeof(*table_perturb), GFP_KERNEL);
+	if (!table_perturb)
+		panic("TCP: failed to alloc table_perturb");
 }
 EXPORT_SYMBOL_GPL(inet_hashinfo_init);
 
