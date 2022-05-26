46f4f1c9fe0d0881ed3c8f026fd7c08de4d4ca2d
roynatech2544 <whiteshell2544@naver.com>
2022-05-26 16:51:16 +0900

    media: fimc-is2: Adopt to commit 5e5d52e8a12b49cb8d85ab47f0d5e45863f45f1d
    Change-Id: Icb2299b1aaf3678f7b5c12f43f226bb3c21fc4a1
    Signed-off-by: roynatech2544 <whiteshell2544@naver.com>

--- drivers/media/platform/exynos/fimc-is2/fimc-is-video-sensor.c
+++ drivers/media/platform/exynos/fimc-is2/fimc-is-video-sensor.c
@@ -938,7 +938,6 @@ const struct v4l2_ioctl_ops fimc_is_ssx_video_ioctl_ops = {
 };
 
 static int fimc_is_ssx_queue_setup(struct vb2_queue *vbq,
-	const void *parg,
 	unsigned int *num_buffers, unsigned int *num_planes,
 	unsigned int sizes[],
 	void *allocators[])
