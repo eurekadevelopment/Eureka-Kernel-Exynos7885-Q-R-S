1e7d8cde13500f572b947bc138aca3667e77eb68
lakkyung.jung <lakkyung.jung@samsung.com>
2023-05-20 15:14:32 +0200

    sched: fair: Modify condition of group_is_overloaded for idle cpu.
    If the idle cpu(CPU_NEWLY_IDLE/CPU_IDLE) performs load balancing,
    the group is considered an overload group if the group has more
    tasks than it can handle.
    
    Change-Id: I25ae84ade65321234ee6e306e7234a238d8e2183
    Signed-off-by: lakkyung.jung <lakkyung.jung@samsung.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8396,6 +8396,9 @@ group_has_capacity(struct lb_env *env, struct sg_lb_stats *sgs)
 	if (sgs->sum_nr_running < sgs->group_weight)
 		return true;
 
+	if (env->idle == CPU_NEWLY_IDLE || env->idle == CPU_IDLE)
+		return true;
+
 	if ((sgs->group_capacity * 100) >
 			(sgs->group_util * env->sd->imbalance_pct))
 		return true;
