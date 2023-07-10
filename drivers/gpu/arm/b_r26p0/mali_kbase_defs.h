e7a94d255b52d1f580716f269dd5e807d009edfa
Sidath Senanayake <sidaths@google.com>
2023-07-10 19:53:00 +0200

    gpu: arm: b_r26p0: mali_kbase: Allow APC support for CSF The struct definition for Asynchronous Power Control was erroneously placed within a non-CSF block which breaks CSF builds. Since this functionality is independent of the front-end type, it has been moved into a section that applies to both CSF and JM builds.
    Bug: 170337464
    Bug: 183242629
    Signed-off-by: Sidath Senanayake <sidaths@google.com>
    Signed-off-by: Dark-Matter7232 <kerneldeveloper7232@gmail.com>
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

--- drivers/gpu/arm/b_r26p0/mali_kbase_defs.h
+++ drivers/gpu/arm/b_r26p0/mali_kbase_defs.h
@@ -1207,17 +1207,6 @@ struct kbase_device {
     struct kthread_worker event_worker;
     struct task_struct *event_worker_thread;
 
-	struct {
-		struct kthread_worker worker;
-		struct task_struct *thread;
-		struct kthread_work power_on_work;
-		struct kthread_work power_off_work;
-		ktime_t end_ts;
-		struct hrtimer timer;
-		bool pending;
-		struct mutex lock;
-	} apc;
-
 	/* See KBASE_JS_*_PRIORITY_MODE for details. */
 	u32 js_ctx_scheduling_mode;
 
@@ -1228,6 +1217,16 @@ struct kbase_device {
 	u8 backup_serialize_jobs;
 #endif /* CONFIG_MALI_CINSTR_GWT */
 
+    struct {
+        struct kthread_worker worker;
+        struct task_struct *thread;
+        struct kthread_work power_on_work;
+        struct kthread_work power_off_work;
+        ktime_t end_ts;
+        struct hrtimer timer;
+        bool pending;
+        struct mutex lock;
+    } apc;
 
 	struct rb_root process_root;
 	struct rb_root dma_buf_root;
