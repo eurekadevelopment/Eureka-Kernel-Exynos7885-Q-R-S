18b2c49653e71d2e663df86221f74e319a8e5be3
ThunderStorms21th <pinakastorm@gmail.com>
2023-07-11 00:24:00 +0200

    sched/fair: Make tunable_scaling more powerfull

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -80,7 +80,7 @@ unsigned int normalized_sysctl_sched_latency = 6000000ULL;
  * SCHED_TUNABLESCALING_LINEAR - scaled linear, *ncpus
  */
 enum sched_tunable_scaling sysctl_sched_tunable_scaling
-	= SCHED_TUNABLESCALING_LOG;
+	= SCHED_TUNABLESCALING_NONE;
 
 /*
  * Minimal preemption granularity for CPU-bound tasks:
