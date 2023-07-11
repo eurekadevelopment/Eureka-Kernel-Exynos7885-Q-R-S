08c535577c6f999cbbdc45e75942388d64a65faa
Siarhei Vishniakou <svv@google.com>
2023-07-11 13:21:19 +0200

    input: touchscreen: Require low latency Currently, CPU can enter deep sleep while waiting for data on the i2c bug. That means an i2c read of 8 bytes can take as long as 10 ms, which would overrun the desired interrupt handling time. Use pm qos to require low latency and prevent CPU from entering deep sleep while handling touch interrupts.
    Bug: 110939384
    Test: look at the i2c read traces:
    1) ./external/chromium-trace/systrace.py --atrace-categories=view,wm,am,app,gfx,i2c,sched,irq,video,power,input,workq,freq
    2) perform a fling
    3) review the i2c read duration on the touchscreen bus
    
    Change-Id: Icf8ab09324003a85af70517769ced3bae52f705c
    Signed-Off-By: Siarhei Vishniakou <svv@google.com>

--- drivers/input/touchscreen/sec_ts/sec_ts.h
+++ drivers/input/touchscreen/sec_ts/sec_ts.h
@@ -31,6 +31,7 @@
 #include <linux/module.h>
 #include <linux/of_gpio.h>
 #include <linux/platform_device.h>
+#include <linux/pm_qos.h>
 #include <linux/regulator/consumer.h>
 #include <linux/slab.h>
 #include <linux/time.h>
@@ -681,6 +682,8 @@ struct sec_ts_data {
 	struct mutex i2c_mutex;
 	struct mutex eventlock;
 	struct mutex modechange;
+
+	struct pm_qos_request pm_qos_req;
 
 	struct delayed_work work_read_info;
 #ifdef USE_POWER_RESET_WORK
