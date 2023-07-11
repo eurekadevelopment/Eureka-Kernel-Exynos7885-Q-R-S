b38d3038e6d3cf5589504d978c49601da660c5a9
Sultan Alsawaf <sultan@kerneltoast.com>
2023-07-11 18:23:54 +0200

    mm: Perform PID map reads on the little CPU cluster PID map reads for processes with thousands of mappings can be done extensively by certain Android apps, burning through CPU time on higher-performance CPUs even though reading PID maps is never a performance-critical task. We can relieve the load on the important CPUs by moving PID map reads to little CPUs via sched_migrate_to_cpumask_*().
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- fs/proc/internal.h
+++ fs/proc/internal.h
@@ -298,6 +298,7 @@ struct proc_maps_private {
 #ifdef CONFIG_NUMA
 	struct mempolicy *task_mempolicy;
 #endif
+	unsigned long old_cpus_allowed;
 };
 
 struct mm_struct *proc_mem_open(struct inode *inode, unsigned int mode);
