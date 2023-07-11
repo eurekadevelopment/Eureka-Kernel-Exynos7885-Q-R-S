9ef49700b53f9521aa258bc104a5ae63ff70a3b8
kevios12 <kerschbaumer.kevin2@gmail.com>
2023-07-11 21:57:15 +0200

    FixUP! mmc: wrong wake_lock_stock doesn't exists

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -2114,7 +2114,7 @@ int mmc_resume_bus(struct mmc_host *host)
 		return -EINVAL;
 
 	printk("%s: Starting deferred resume\n", mmc_hostname(host));
-	wake_lock_stock(&host->detect_wake_lock);
+	wake_lock(&host->detect_wake_lock);
 	spin_lock_irqsave(&host->lock, flags);
 	host->bus_resume_flags &= ~MMC_BUSRESUME_NEEDS_RESUME;
 	host->rescan_disable = 0;
