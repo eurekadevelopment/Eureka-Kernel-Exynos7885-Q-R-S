5d8fea8399c9e181fc4460764bd748d8e7afae6b
Sultan Alsawaf <sultan@kerneltoast.com>
2023-07-11 15:16:55 +0200

    drm: Reduce latency while processing atomic ioctls Unfortunately, our display driver must occasionally sleep inside start_atomic(), which causes the CPU running the ioctl to schedule and potentially idle. Depending on how deeply the running CPU idles, there can be quite a bit of latency added to processing the "atomic" ioctl as a result, which hurts display rendering latency.
    We can mitigate this effect somewhat by forcing the CPU running the
    ioctl to stick to shallow idle states. This is done by forcing the task
    running the ioctl to stick to its current CPU and then using PM QoS to
    keep said CPU from entering idle states that take more than 100 us to
    exit.
    
    We can also further reduce the rendering latency by elevating the
    priority of the task running the ioctl, so that after it goes to sleep,
    it will wake up sooner when the wait inside start_atomic() is over.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

--- drivers/gpu/drm/drm_atomic.c
+++ drivers/gpu/drm/drm_atomic.c
@@ -29,6 +29,8 @@
 #include <drm/drmP.h>
 #include <drm/drm_atomic.h>
 #include <drm/drm_plane_helper.h>
+#include <drm/drm_print.h>
+#include <linux/pm_qos.h>
 
 /**
  * drm_atomic_state_default_release -
@@ -1477,8 +1479,8 @@ void drm_atomic_clean_old_fb(struct drm_device *dev,
 }
 EXPORT_SYMBOL(drm_atomic_clean_old_fb);
 
-int drm_mode_atomic_ioctl(struct drm_device *dev,
-			  void *data, struct drm_file *file_priv)
+static int __drm_mode_atomic_ioctl(struct drm_device *dev, void *data,
+				   struct drm_file *file_priv)
 {
 	struct drm_mode_atomic *arg = data;
 	uint32_t __user *objs_ptr = (uint32_t __user *)(unsigned long)(arg->objs_ptr);
@@ -1652,5 +1654,47 @@ out:
 	drm_modeset_drop_locks(&ctx);
 	drm_modeset_acquire_fini(&ctx);
 
+	return ret;
+}
+
+void set_cpus_allowed_common(struct task_struct *p,
+			     const struct cpumask *new_mask);
+
+int drm_mode_atomic_ioctl(struct drm_device *dev, void *data,
+			  struct drm_file *file_priv)
+{
+	struct cpumask old_cpus_allowed;
+	struct pm_qos_request req;
+	unsigned int cpu;
+	long old_nice;
+	int ret;
+
+	preempt_disable();
+
+	/* Don't let the current task migrate to another CPU */
+	cpu = raw_smp_processor_id();
+	cpumask_copy(&old_cpus_allowed, &current->cpus_allowed);
+	set_cpus_allowed_common(current, cpumask_of(cpu));
+
+	/* Elevate the priority of the current process */
+	old_nice = task_nice(current);
+	set_user_nice(current, MIN_NICE);
+
+	/* Don't let this CPU use deep idle states while the ioctl runs */
+	req = (typeof(req)){
+		.type = PM_QOS_REQ_AFFINE_CORES,
+		.cpus_affine = ATOMIC_INIT(BIT(cpu))
+	};
+	pm_qos_add_request(&req, PM_QOS_CPU_DMA_LATENCY, 100);
+	preempt_enable_no_resched();
+
+	ret = __drm_mode_atomic_ioctl(dev, data, file_priv);
+
+	/* Restore everything back to normal */
+	preempt_disable();
+	pm_qos_remove_request(&req);
+	set_user_nice(current, old_nice);
+	set_cpus_allowed_common(current, &old_cpus_allowed);
+	preempt_enable_no_resched();
 	return ret;
 }
