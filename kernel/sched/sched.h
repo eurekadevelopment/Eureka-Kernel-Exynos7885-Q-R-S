5bd372db468c57b96f4e7cf45b87645d89a3a9be
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2023-08-17 18:31:29 +0200

    irqwork: push most work into softirq context
    Initially we defered all irqwork into softirq because we didn't want the
    latency spikes if perf or another user was busy and delayed the RT task.
    The NOHZ trigger (nohz_full_kick_work) was the first user that did not work
    as expected if it did not run in the original irqwork context so we had to
    bring it back somehow for it. push_irq_work_func is the second one that
    requires this.
    
    This patch adds the IRQ_WORK_HARD_IRQ which makes sure the callback runs
    in raw-irq context. Everything else is defered into softirq context. Without
    -RT we have the orignal behavior.
    
    This patch incorporates tglx orignal work which revoked a little bringing back
    the arch_irq_work_raise() if possible and a few fixes from Steven Rostedt and
    Mike Galbraith,
    
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -481,12 +481,6 @@ struct rt_rq {
 	atomic_long_t removed_util_avg;
 	atomic_long_t removed_load_avg;
 
-#ifdef HAVE_RT_PUSH_IPI
-	int push_flags;
-	int push_cpu;
-	struct irq_work push_work;
-	raw_spinlock_t push_lock;
-#endif
 #endif /* CONFIG_SMP */
 	int rt_queued;
 
@@ -574,6 +568,19 @@ struct root_domain {
 	struct dl_bw dl_bw;
 	struct cpudl cpudl;
 
+#ifdef HAVE_RT_PUSH_IPI
+	/*
+	 * For IPI pull requests, loop across the rto_mask.
+	 */
+	struct irq_work rto_push_work;
+	raw_spinlock_t rto_lock;
+	/* These are only updated and read within rto_lock */
+	int rto_loop;
+	int rto_cpu;
+	/* These atomics are updated outside of a lock */
+	atomic_t rto_loop_next;
+	atomic_t rto_loop_start;
+#endif
 	/*
 	 * The "RT overload" flag: it gets set if a CPU has more than
 	 * one runnable RT task.
@@ -586,6 +593,9 @@ extern struct root_domain def_root_domain;
 extern void sched_get_rd(struct root_domain *rd);
 extern void sched_put_rd(struct root_domain *rd);
 
+#ifdef HAVE_RT_PUSH_IPI
+extern void rto_push_irq_work_func(struct irq_work *work);
+#endif
 #endif /* CONFIG_SMP */
 
 /*
