b29914ce54316be4358fe57a0c2e5613d173fba1
Thomas Gleixner <tglx@linutronix.de>
2023-08-17 15:31:56 +0200

    fs: namespace preemption fix
    On RT we cannot loop with preemption disabled here as
    mnt_make_readonly() might have been preempted. We can safely enable
    preemption while waiting for MNT_WRITE_HOLD to be cleared. Safe on !RT
    as well.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- fs/namespace.c
+++ fs/namespace.c
@@ -571,9 +571,13 @@ int __mnt_want_write(struct vfsmount *m)
 #ifdef CONFIG_RKP_NS_PROT
 	while (ACCESS_ONCE(mnt->mnt->mnt_flags) & MNT_WRITE_HOLD)
 #else
-	while (ACCESS_ONCE(mnt->mnt.mnt_flags) & MNT_WRITE_HOLD)
+	while (ACCESS_ONCE(mnt->mnt.mnt_flags) & MNT_WRITE_HOLD) {
+		preempt_enable();
+ 		cpu_relax();
+		preempt_disable();
+	}
 #endif
-		cpu_relax();
+
 	/*
 	 * After the slowpath clears MNT_WRITE_HOLD, mnt_is_readonly will
 	 * be set to match its requirements. So we must not load that until
