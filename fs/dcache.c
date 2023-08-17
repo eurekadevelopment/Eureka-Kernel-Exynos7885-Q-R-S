bf18ab24f7df31d280a58b875ee99ea95d83cb69
Thomas Gleixner <tglx@linutronix.de>
2023-08-17 16:15:19 +0200

    fs: dcache: Use cpu_chill() in trylock loops
    Retry loops on RT might loop forever when the modifying side was
    preempted. Use cpu_chill() instead of cpu_relax() to let the system
    make progress.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- fs/dcache.c
+++ fs/dcache.c
@@ -19,6 +19,7 @@
 #include <linux/mm.h>
 #include <linux/fs.h>
 #include <linux/fsnotify.h>
+#include <linux/delay.h>
 #include <linux/slab.h>
 #include <linux/init.h>
 #include <linux/hash.h>
@@ -822,7 +823,12 @@ repeat:
 kill_it:
 	dentry = dentry_kill(dentry);
 	if (dentry) {
-		cond_resched();
+		int r;
+
+		/* the task with the highest priority won't schedule */
+		r = cond_resched();
+		if (!r)
+			cpu_chill();
 		goto repeat;
 	}
 }
@@ -2453,7 +2459,7 @@ again:
 	if (dentry->d_lockref.count == 1) {
 		if (!spin_trylock(&inode->i_lock)) {
 			spin_unlock(&dentry->d_lock);
-			cpu_relax();
+			cpu_chill();
 			goto again;
 		}
 		dentry->d_flags &= ~DCACHE_CANT_MOUNT;
