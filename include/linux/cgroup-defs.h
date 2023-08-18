cbfb82e0f8b19e58470943c1685058fc05e40f5d
Mike Galbraith <umgwanakikbuti@gmail.com>
2023-08-18 10:29:36 +0200

    work-simple: Rename work-simple.[ch] to swork.[ch]  for consistency
    The internal bits are already swork_blah, rename source to match.
    
    Signed-off-by: Mike Galbraith <umgwanakikbuti@gmail.com>
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>

--- include/linux/cgroup-defs.h
+++ include/linux/cgroup-defs.h
@@ -16,7 +16,7 @@
 #include <linux/percpu-refcount.h>
 #include <linux/percpu-rwsem.h>
 #include <linux/workqueue.h>
-#include <linux/work-simple.h>
+#include <linux/swork.h>
 #include <linux/bpf-cgroup.h>
 
 #ifdef CONFIG_CGROUPS
