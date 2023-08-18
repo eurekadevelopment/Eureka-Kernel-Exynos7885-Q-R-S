ae5395bbfd58ff98311ec9a862bac09012f0eba9
Andrzej Perczak <linux@andrzejperczak.com>
2023-08-18 21:08:54 +0200

    Note that dirty_writeback_centisecs and drity_background_ratio have to be restricted from writing because Android < 12 modifies these values badly on boot.
    Signed-off-by: Andrzej Perczak <linux@andrzejperczak.com>

--- mm/swap.c
+++ mm/swap.c
@@ -1183,7 +1183,6 @@ EXPORT_SYMBOL(pagevec_lookup_range_nr_tag);
  */
 void __init swap_setup(void)
 {
-	unsigned long megs = totalram_pages >> (20 - PAGE_SHIFT);
 #ifdef CONFIG_SWAP
 	int i;
 
@@ -1191,11 +1190,9 @@ void __init swap_setup(void)
 		spin_lock_init(&swapper_spaces[i].tree_lock);
 #endif
 
-	/* Use a smaller cluster for small-memory machines */
-	if (megs < 16)
-		page_cluster = 2;
-	else
-		page_cluster = 0;
+	/* Tweak for Android devices using zram */
+	page_cluster = 0;
+
 	/*
 	 * Right now other parts of the system means that we
 	 * _really_ don't want to cluster much more
