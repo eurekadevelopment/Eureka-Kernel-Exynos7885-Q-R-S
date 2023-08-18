3815517bdc777595f73d499a8cb10e252bc755cc
celtare21 <celtare21@gmail.com>
2023-08-18 21:48:46 +0200

    kernel: alarmtimer: Relax wakelock

--- kernel/time/alarmtimer.c
+++ kernel/time/alarmtimer.c
@@ -256,7 +256,7 @@ static int alarmtimer_suspend(struct device *dev)
 		return 0;
 
 	if (ktime_to_ns(min) < 2 * NSEC_PER_SEC)
-		__pm_wakeup_event(ws, ktime_to_ms(min) + 1);
+		__pm_wakeup_event(ws, MSEC_PER_SEC / 2);
 
 	/* Setup an rtc timer to fire that far in the future */
 	rtc_timer_cancel(rtc, &rtctimer);
