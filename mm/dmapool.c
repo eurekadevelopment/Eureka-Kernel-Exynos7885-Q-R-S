003b20416f1c6f7422bd7efdd139429e2cbe3b90
Keith Busch <kbusch@kernel.org>
2023-08-20 20:19:01 +0200

    dmapool: don't memset on free twice If debug is enabled, dmapool will poison the range, so no need to clear it to 0 immediately before writing over it.
    Link: https://lkml.kernel.org/r/20230126215125.4069751-11-kbusch@meta.com
    Fixes: 2d55c16c0c54 ("dmapool: create/destroy cleanup")
    Signed-off-by: Keith Busch <kbusch@kernel.org>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Cc: Matthew Wilcox <willy@infradead.org>
    Cc: Tony Battersby <tonyb@cybernetics.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/dmapool.c
+++ mm/dmapool.c
@@ -163,6 +163,8 @@ static void pool_check_block(struct dma_pool *pool, void *retval,
 static bool pool_page_err(struct dma_pool *pool, struct dma_page *page,
 			  void *vaddr, dma_addr_t dma)
 {
+	if (want_init_on_free())
+		memset(vaddr, 0, pool->size);
 	return false;
 }
 
