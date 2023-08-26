d011c242d0d0bbc567f1824e0bcdd2b88cdc2e24
kevios12 <kerschbaumer.kevin2@gmail.com>
2023-08-26 19:50:46 +0200

    mm: increase vm_min_readhead to 64kb

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -2049,7 +2049,7 @@ void task_dirty_inc(struct task_struct *tsk);
 
 /* readahead.c */
 #define VM_MAX_READAHEAD	128	/* kbytes */
-#define VM_MIN_READAHEAD	16	/* kbytes (includes current page) */
+#define VM_MIN_READAHEAD	64	/* kbytes (includes current page) */
 extern int mmap_readaround_limit;
 
 int force_page_cache_readahead(struct address_space *mapping, struct file *filp,
