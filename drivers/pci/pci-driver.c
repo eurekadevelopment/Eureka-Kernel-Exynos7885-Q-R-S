dcab32ba2704af88f524eb0305b86dd073ac197a
kevios12 <kerschbaumer.kevin2@gmail.com>
2023-08-31 14:45:18 +0200

    FixUP! PCI / PM: Avoid resuming more devices during system suspend

--- drivers/pci/pci-driver.c
+++ drivers/pci/pci-driver.c
@@ -684,8 +684,13 @@ static int pci_pm_prepare(struct device *dev)
 
 static void pci_pm_complete(struct device *dev)
 {
-	pci_dev_complete_resume(to_pci_dev(dev));
-	pm_complete_with_resume_check(dev);
+	struct device_driver *drv = dev->driver;
+	struct pci_dev *pci_dev = to_pci_dev(dev);
+
+	pci_dev_complete_resume(pci_dev);
+
+	if (drv && drv->pm && drv->pm->complete)
+		drv->pm->complete(dev);
 }
 
 #else /* !CONFIG_PM_SLEEP */
