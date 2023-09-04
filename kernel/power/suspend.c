4709efbc5b25d41f4298406100564fdaaecea31e
EmanuelCN <emanuelghub@gmail.com>
2023-09-04 16:04:42 +0200

    kernel/power/suspend: Use deepsleep by default

--- kernel/power/suspend.c
+++ kernel/power/suspend.c
@@ -48,7 +48,7 @@ static const char * const mem_sleep_labels[] = {
 };
 const char *mem_sleep_states[PM_SUSPEND_MAX];
 
-suspend_state_t mem_sleep_current = PM_SUSPEND_FREEZE;
+suspend_state_t mem_sleep_current = PM_SUSPEND_MEM;
 static suspend_state_t mem_sleep_default = PM_SUSPEND_MEM;
 suspend_state_t pm_suspend_target_state;
 EXPORT_SYMBOL_GPL(pm_suspend_target_state);
