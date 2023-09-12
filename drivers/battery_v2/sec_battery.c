0d459ee0be190db2e7f28bd9884263b3cf5a6a4d
morogoku <morogoku@hotmail.com>
2023-09-12 21:27:42 +0200

    sec_battery: Add Wakelock control Signed-off-by: morogoku <morogoku@hotmail.com>

--- drivers/battery_v2/sec_battery.c
+++ drivers/battery_v2/sec_battery.c
@@ -25,6 +25,11 @@
 #include <linux/sti/abc_common.h>
 #endif
 
+#include <linux/moduleparam.h>
+
+static int wl_polling = 10;
+module_param(wl_polling, int, 0644);
+
 bool sleep_mode = false;
 
 static struct device_attribute sec_battery_attrs[] = {
@@ -1486,7 +1491,7 @@ static bool sec_bat_ovp_uvlo_result(
 			/* Take the wakelock during 10 seconds
 			 * when over-voltage status is detected
 			 */
-			wake_lock_timeout(&battery->vbus_wake_lock, HZ * 10);
+			wake_lock_timeout(&battery->vbus_wake_lock, HZ * wl_polling);
 			break;
 		}
 		power_supply_changed(battery->psy_bat);
@@ -2802,7 +2807,7 @@ static void sec_bat_do_fullcharged(
 	 * activated wake lock in a few seconds
 	 */
 	if (battery->pdata->polling_type == SEC_BATTERY_MONITOR_ALARM)
-		wake_lock_timeout(&battery->vbus_wake_lock, HZ * 10);
+		wake_lock_timeout(&battery->vbus_wake_lock, HZ * wl_polling);
 }
 
 static bool sec_bat_fullcharged_check(
@@ -3545,7 +3550,7 @@ static void sec_bat_fw_update_work(struct sec_battery_info *battery, int mode)
 
 	dev_info(battery->dev, "%s\n", __func__);
 
-	wake_lock_timeout(&battery->vbus_wake_lock, HZ * 10);
+	wake_lock_timeout(&battery->vbus_wake_lock, HZ * wl_polling);
 
 	switch (mode) {
 	case SEC_WIRELESS_RX_SDCARD_MODE:
@@ -4033,7 +4038,7 @@ skip_monitor:
 	sec_bat_set_polling(battery);
 
 	if (battery->capacity <= 0 || battery->health_change)
-		wake_lock_timeout(&battery->monitor_wake_lock, HZ * 5);
+		wake_lock_timeout(&battery->monitor_wake_lock, HZ * wl_polling);
 	else
 		wake_unlock(&battery->monitor_wake_lock);
 
@@ -4240,7 +4245,7 @@ static void sec_bat_cable_work(struct work_struct *work)
 	 * if cable is connected and disconnected,
 	 * activated wake lock in a few seconds
 	 */
-	wake_lock_timeout(&battery->vbus_wake_lock, HZ * 10);
+	wake_lock_timeout(&battery->vbus_wake_lock, HZ * wl_polling);
 
 	if (battery->cable_type == SEC_BATTERY_CABLE_NONE ||
 		((battery->pdata->cable_check_type &
