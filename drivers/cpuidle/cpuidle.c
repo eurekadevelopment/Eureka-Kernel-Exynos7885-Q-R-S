defd60d86cca93a9bcdbdf786c2d5e557757d7e7
kevios12 <kerschbaumer.kevin2@gmail.com>
2023-09-12 19:15:51 +0200

    cpuidle: move curr gov code to the correct placement

--- drivers/cpuidle/cpuidle.c
+++ drivers/cpuidle/cpuidle.c
@@ -442,6 +442,9 @@ int cpuidle_enable_device(struct cpuidle_device *dev)
 	if (dev->enabled)
 		return 0;
 
+	if (!cpuidle_curr_governor)
+		return -EIO;
+
 	drv = cpuidle_get_cpu_driver(dev);
 
 	if (!drv || !cpuidle_curr_governor)
