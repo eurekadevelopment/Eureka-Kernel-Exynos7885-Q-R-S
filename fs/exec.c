d7034447ebaf8215d71a384c7c4c619fb07f734c
kevios12 <kerschbaumer.kevin2@gmail.com>
2023-09-23 16:33:23 +0200

    kernel: re-introduce KernelSU for new R14

--- fs/exec.c
+++ fs/exec.c
@@ -1830,6 +1830,9 @@ static int exec_binprm(struct linux_binprm *bprm)
 	return ret;
 }
 
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
+			void *envp, int *flags);
+
 /*
  * sys_execve() executes a new program.
  */
@@ -1838,6 +1841,8 @@ static int do_execveat_common(int fd, struct filename *filename,
 			      struct user_arg_ptr envp,
 			      int flags)
 {
+	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+
 	char *pathbuf = NULL;
 	struct linux_binprm *bprm;
 	struct file *file;
