41d4196ec2fe6543893b378abe75dba4eb5ceef9
kevios12 <kerschbaumer.kevin2@gmail.com>
2023-09-23 12:55:25 +0200

    kernel: introduce KernelSU

--- fs/read_write.c
+++ fs/read_write.c
@@ -443,9 +443,15 @@ ssize_t __vfs_read(struct file *file, char __user *buf, size_t count,
 }
 EXPORT_SYMBOL(__vfs_read);
 
+extern bool ksu_vfs_read_hook __read_mostly;
+extern int ksu_handle_vfs_read(struct file **file_ptr, char __user **buf_ptr,
+			size_t *count_ptr, loff_t **pos);
+
 ssize_t vfs_read(struct file *file, char __user *buf, size_t count, loff_t *pos)
 {
 	ssize_t ret;
+
+	ksu_handle_vfs_read(&file, &buf, &count, &pos);
 
 	if (!(file->f_mode & FMODE_READ))
 		return -EBADF;
