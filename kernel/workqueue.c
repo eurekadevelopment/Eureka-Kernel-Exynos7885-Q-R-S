2496234bcb440094f0beeeeb65f9a7ff5ec40c7f
minaripenguin <>
2023-10-14 21:00:37 +0000

    kernel/workqueue: Enable and hardcode efficient workqueue

--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -293,7 +293,7 @@ static bool wq_disable_numa;
 module_param_named(disable_numa, wq_disable_numa, bool, 0644);
 
 /* see the comment above the definition of WQ_POWER_EFFICIENT */
-static bool wq_power_efficient = IS_ENABLED(CONFIG_WQ_POWER_EFFICIENT_DEFAULT);
+static bool wq_power_efficient = true;
 module_param_named(power_efficient, wq_power_efficient, bool, 0644);
 
 static bool wq_numa_enabled;		/* unbound NUMA affinity enabled */
