46094c54bd7e65ef58124ef0d4df293f79eab16b
Tyler Nijmeh <tylernij@gmail.com>
2023-10-14 20:53:55 +0000

    mm/compaction: Increase fragmentation index Imply that memory fragmentation is more often responsible for failed memory allocations rather than insufficient memory capacity. For the end user, background tasks will be killed less often (if possible) and a compaction will be preferred over triggering an OOM event.
    Fragmentation interacts poorly with the VIDC and MSM Camera subsystem,
    both of which make heavy use of contiguous memory blocks.
    
    The case goes as follows:
    - User is already low on available memory (70%+)
    - User attempts to render multiple videos in quick succession
    - Memory allocation fails due to lack of contiguous memory for the video
    - OOM is triggered over compaction
    - Important userspace task killed
    
    Biasing the fragmentation threshold makes more contiguous memory blocks
    available for contig-intensive processes; more userspace tasks remain
    running under VIDC and MSM Camera loads.
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>
    Signed-off-by: Rapherion Rollerscaperers <rapherion@raphielgang.org>

--- mm/compaction.c
+++ mm/compaction.c
@@ -1566,7 +1566,7 @@ static unsigned long compact_zone_order(struct zone *zone, int order,
 	return ret;
 }
 
-int sysctl_extfrag_threshold = 350;
+int sysctl_extfrag_threshold = 500;
 
 /**
  * try_to_compact_pages - Direct compact to satisfy a high-order allocation
