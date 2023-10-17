b9ee6228c905981d1f7d3314dc3cd96e44a41ce1
luk1337 <priv.luk@gmail.com>
2023-10-17 17:24:46 +0200

    ext4: Add no_sehash_xattr mount option * Useful for devices where /persist may have unexpected SELinux   contexts but xattr of root directory is valid, leading to   restorecon early exitting without traversing the partition.
    Change-Id: I5089ff90f76aa9f3db7da26f73548cf62fe67bd0

--- fs/ext4/xattr_security.c
+++ fs/ext4/xattr_security.c
@@ -35,6 +35,8 @@ ext4_xattr_security_get(const struct xattr_handler *handler,
 {
 	if (strcmp(name, "") == 0)
 		return -EINVAL;
+	if (strcmp(name, "sehash") == 0 && test_opt(dentry->d_sb, NO_SEHASH_XATTR))
+		return 0;
 	return ext4_xattr_get(d_inode(dentry), EXT4_XATTR_INDEX_SECURITY,
 			      name, buffer, size);
 }
@@ -46,6 +48,8 @@ ext4_xattr_security_set(const struct xattr_handler *handler,
 {
 	if (strcmp(name, "") == 0)
 		return -EINVAL;
+	if (strcmp(name, "sehash") == 0 && test_opt(dentry->d_sb, NO_SEHASH_XATTR))
+		return 0;
 	return ext4_xattr_set(d_inode(dentry), EXT4_XATTR_INDEX_SECURITY,
 			      name, value, size, flags);
 }
