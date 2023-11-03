d3f35aa53d0c564e5ee744f7f7756e33b8dba6b8
Sarthak Roy <sarthak.roy@s.amity.edu>
2023-11-03 20:48:57 +0100

    mm/ksm: optimize kernel samepage merger to gain more battery

--- mm/ksm.c
+++ mm/ksm.c
@@ -218,10 +218,10 @@ static unsigned long ksm_pages_unshared;
 static unsigned long ksm_rmap_items;
 
 /* Number of pages ksmd should scan in one batch */
-static unsigned int ksm_thread_pages_to_scan = 100;
+static unsigned int ksm_thread_pages_to_scan = 250;
 
 /* Milliseconds ksmd should sleep between batches */
-static unsigned int ksm_thread_sleep_millisecs = 20;
+static unsigned int ksm_thread_sleep_millisecs = 1500;
 
 #ifdef CONFIG_NUMA
 /* Zeroed when merging across nodes is not allowed */
