206cd97e99f3009a84e283925396242fc1559b09
kholk <angelogioacchino.delregno@somainline.org>
2023-11-08 23:47:02 +0100

    Backport new vmalloc for "large performance benefits" This is a backport from Linux 5.2-rc1 of a patch series to greatly enhance vmalloc's performance especially on embedded systems, plus all of its dependencies that were missing in kernel 4.9.
    For all the informations, refer to LKML: https://lkml.org/lkml/2018/10/19/786
    
    Brief informations:
    Currently an allocation of the new VA area is done over busy list iteration until a suitable hole
    is found between two busy areas. Therefore each new allocation causes the list being grown.
    Due to long list and different permissive parameters an allocation can take a long time on
    embedded devices(milliseconds).
    
    This patch organizes the vmalloc memory layout into free areas of the VMALLOC_START-VMALLOC_END range.
    It uses a red-black tree that keeps blocks sorted by their offsets in pair with linked list
    keeping the free space in order of increasing addresses.
    
    Quote Phoronix:
    With this patch from Uladzislau Rezki, calling vmalloc() can take up to 67% less time compared
    to the behavior on Linux 5.1 and prior, at least with tests done by the developer under QEMU.
    
    Personal tests are showing that the device is more responsive when memory pressure is
    high and when huge allocations are to be done, it's also noticeably faster in this case, like
    when starting Chrome with more than 100 opened tabs after a system reboot (so, an
    uncached complete load of it).
    
    Shameless kanged from:
    https://github.com/sonyxperiadev/kernel / Pull Request 2016
    
    Pzqqt: Backport to v4.4
    Signed-off-by: Pzqqt <821026875@qq.com>
    Signed-off-by: RyuujiX <saputradenny712@gmail.com>

--- drivers/parisc/ccio-dma.c
+++ drivers/parisc/ccio-dma.c
@@ -1267,7 +1267,7 @@ ccio_ioc_init(struct ioc *ioc)
 	** Hot-Plug/Removal of PCI cards. (aka PCI OLARD).
 	*/
 
-	iova_space_size = (u32) (totalram_pages / count_parisc_driver(&ccio_driver));
+	iova_space_size = (u32) (totalram_pages() / count_parisc_driver(&ccio_driver));
 
 	/* limit IOVA space size to 1MB-1GB */
 
@@ -1306,11 +1306,11 @@ ccio_ioc_init(struct ioc *ioc)
 
 	DBG_INIT("%s() hpa 0x%p mem %luMB IOV %dMB (%d bits)\n",
 			__func__, ioc->ioc_regs,
-			(unsigned long) totalram_pages >> (20 - PAGE_SHIFT),
+			(unsigned long) totalram_pages() >> (20 - PAGE_SHIFT),
 			iova_space_size>>20,
 			iov_order + PAGE_SHIFT);
 
-	ioc->pdir_base = (u64 *)__get_free_pages(GFP_KERNEL, 
+	ioc->pdir_base = (u64 *)__get_free_pages(GFP_KERNEL,
 						 get_order(ioc->pdir_size));
 	if(NULL == ioc->pdir_base) {
 		panic("%s() could not allocate I/O Page Table\n", __func__);
