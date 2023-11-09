4ca9d60a459a635cbd3a1e98ecd63ca90068a790
Sultan Alsawaf <sultan@kerneltoast.com>
2023-11-09 12:15:06 +0100

    kernel: Fix cpufreq times memory leaks The segregation between cpufreq_task_times_init and cpufreq_task_times_alloc is unnecessary and is causing memory leaks to occur since cpufreq_task_times_init wipes out allocated pointers when idle threads are created.
    To fix the leaks, just use a single cpufreq_task_times_init that handles
    allocating memory for the cpufreq times at the moment that a task struct
    is duplicated.
    
    This fixes the following leaks:
    unreferenced object 0xffffffd6f6d37580 (size 128):
      comm "swapper/0", pid 1, jiffies 4294877304 (age 54.670s)
      hex dump (first 32 bytes):
        00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00  ................
        00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00  ................
      backtrace:
        [<ffffff9681bfb81c>] __kmalloc+0x1a4/0x2f8
        [<ffffff9682521748>] cpufreq_task_times_alloc+0x80/0xe8
        [<ffffff9681adaf80>] sched_fork+0x50/0x2c0
        [<ffffff9681aa4a64>] copy_process.isra.7.part.8+0x354/0x14a0
        [<ffffff9681aa5c1c>] fork_idle+0x3c/0x88
        [<ffffff9683a0a544>] idle_threads_init+0x9c/0xe0
        [<ffffff9683a11bac>] smp_init+0x3c/0xe8
        [<ffffff9683a00f40>] kernel_init_freeable+0xd0/0x21c
        [<ffffff9682d27eb4>] kernel_init+0x10/0xf4
        [<ffffff9681a82ef0>] ret_from_fork+0x10/0x20
        [<ffffffffffffffff>] 0xffffffffffffffff
    
    unreferenced object 0xffffffd6f6d37600 (size 128):
      comm "swapper/0", pid 1, jiffies 4294877304 (age 54.670s)
      hex dump (first 32 bytes):
        00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00  ................
        00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00  ................
      backtrace:
        [<ffffff9681bfb81c>] __kmalloc+0x1a4/0x2f8
        [<ffffff968252177c>] cpufreq_task_times_alloc+0xb4/0xe8
        [<ffffff9681adaf80>] sched_fork+0x50/0x2c0
        [<ffffff9681aa4a64>] copy_process.isra.7.part.8+0x354/0x14a0
        [<ffffff9681aa5c1c>] fork_idle+0x3c/0x88
        [<ffffff9683a0a544>] idle_threads_init+0x9c/0xe0
        [<ffffff9683a11bac>] smp_init+0x3c/0xe8
        [<ffffff9683a00f40>] kernel_init_freeable+0xd0/0x21c
        [<ffffff9682d27eb4>] kernel_init+0x10/0xf4
        [<ffffff9681a82ef0>] ret_from_fork+0x10/0x20
        [<ffffffffffffffff>] 0xffffffffffffffff
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Peppe289 <gsperanza204@gmail.com>
    Signed-off-by: RyuujiX <saputradenny712@gmail.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -2050,8 +2050,6 @@ long _do_fork(unsigned long clone_flags,
 		struct completion vfork;
 		struct pid *pid;
 
-		cpufreq_task_times_alloc(p);
-
 		trace_sched_process_fork(current, p);
 
 		pid = get_task_pid(p, PIDTYPE_PID);
