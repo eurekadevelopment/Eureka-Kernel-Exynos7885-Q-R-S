809a834decf0f12c93bc87d43aa7d28b3ec8fda6
Danny Lin <danny@kdrag0n.dev>
2023-11-12 20:40:07 +0100

    exec: Add node tampering blacklist function We'll be adding checks to block writes from processes which tamper with values that we control from within the kernel, especially ones that userspace writes to for boosting. Add a central function to perform the process check to reduce code duplication.
    This blacklists the following processes which are known to tamper with
    such values:
      - init
      - libperfmgr (power@1.3-servi and NodeLooperThrea)
      - perfd (perf@1.0-servic)
      - init.qcom.post_boot.sh (init.qcom.post_)
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    [added libperfmgr 1.2 in case some ROMs use wahoo powerhal]
    Signed-off-by: Volodymyr Zhdanov <wight554@gmail.com>
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- include/linux/binfmts.h
+++ include/linux/binfmts.h
@@ -121,4 +121,17 @@ extern void set_binfmt(struct linux_binfmt *new);
 extern ssize_t read_code(struct file *, unsigned long, loff_t, size_t);
 extern bool is_zygote_pid(pid_t pid);
 
+static inline bool task_is_booster(struct task_struct *tsk)
+{
+	char comm[sizeof(tsk->comm)];
+
+	get_task_comm(comm, tsk);
+	return !strcmp(comm, "init") || !strcmp(comm, "NodeLooperThrea") ||
+               !strcmp(comm, "power@-servi") ||
+	       !strcmp(comm, "power@1.0-servi") ||
+	       !strcmp(comm, "power@1.2-servi") ||
+	       !strcmp(comm, "power@1.3-servi") ||
+	       !strcmp(comm, "perf@1.0-servic");
+}
+
 #endif /* _LINUX_BINFMTS_H */
