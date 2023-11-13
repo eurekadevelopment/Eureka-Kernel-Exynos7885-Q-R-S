0d299096a223f1be58346872cdb84c182f2dd9b3
celtare21 <celtare21@gmail.com>
2023-11-13 21:35:09 +0100

    sched: Import sched_irq_work_queue() Signed-off-by: celtare21 <celtare21@gmail.com> Signed-off-by: Little-W <1405481963@qq.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -2007,3 +2007,13 @@ static inline unsigned long cpu_util(int cpu)
 }
 #endif
 #endif
+
+#ifdef CONFIG_SMP
+static inline void sched_irq_work_queue(struct irq_work *work)
+{
+	if (likely(cpu_online(raw_smp_processor_id())))
+		irq_work_queue(work);
+	else
+		irq_work_queue_on(work, cpumask_any(cpu_online_mask));
+}
+#endif
