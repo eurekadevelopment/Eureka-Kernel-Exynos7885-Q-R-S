7881c734ea59e97d62e56b24e7f2ab4960d5f474
roynatech2544 <whiteshell2544@naver.com>
2023-12-01 14:02:58 +0000

    regen_defconfig: Do not force LLVM=1
    - Some hosts may not have clang installed

--- regen_defconfig.sh
+++ regen_defconfig.sh
@@ -8,8 +8,8 @@ MERGE_FRAGMENT=
 fi
 
 for def in arch/arm64/configs/exynos7885-*; do
-	make O=out LLVM=1 $(basename $def) $MERGE_FRAGMENT;
+	make O=out $(basename $def) $MERGE_FRAGMENT;
 	cp out/.config $(dirname $def)/full/$(basename $def);
-	make O=out LLVM=1 savedefconfig;
+	make O=out savedefconfig;
 	cp out/defconfig $def;
 done
