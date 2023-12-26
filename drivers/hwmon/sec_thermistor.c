56eda3fe7e81b55e3994ad0cbcf972ed62b8c816
Soo Hwan Na <whiteshell2544@naver.com>
2023-12-26 05:07:26 +0000

    Revert "drivers/hwmon: Add option to disable throttling"
    This reverts commit 2a4742ecbd93e66e9d02c48de7c58770418830b5.

--- drivers/hwmon/sec_thermistor.c
+++ drivers/hwmon/sec_thermistor.c
@@ -202,11 +202,7 @@ static int convert_adc_to_temper(struct sec_therm_info *info, unsigned int adc)
 		(info->pdata->adc_table[low].adc -
 			info->pdata->adc_table[high].adc);
 
-#ifdef CONFIG_NO_THROTTLING 
-	return 200;
-#else
 	return temp;
-#endif
 }
 
 #define ABS(x)		((x) < 0 ? (-1 * (x)) : (x))
