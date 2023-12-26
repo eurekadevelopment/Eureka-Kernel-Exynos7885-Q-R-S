81eab27ab1cf8a53e528b57fd2cda436db54f221
Soo Hwan Na <whiteshell2544@naver.com>
2023-12-26 03:25:27 +0000

    Revert "kernel: Affine Samsung's Wi-Fi daemon and HAL to the big CPU cluster"
    This reverts commit 536eff6f684b2fce7dc0d8fdb6233a267654718f.

--- fs/exec.c
+++ fs/exec.c
@@ -88,9 +88,6 @@ int suid_dumpable = 0;
 static LIST_HEAD(formats);
 static DEFINE_RWLOCK(binfmt_lock);
 
-#define SEC_WLBTD_BIN_PREFIX "/vendor/bin/wlbtd"
-#define SEC_WLAN_HAL_BIN_PREFIX "/vendor/bin/hw/vendor.samsung.hardware.wifi"
-
 #define ZYGOTE32_BIN "/system/bin/app_process32"
 #define ZYGOTE64_BIN "/system/bin/app_process64"
 static pid_t zygote32_pid;
@@ -1977,14 +1974,6 @@ static int do_execveat_common(int fd, struct filename *filename,
 			zygote32_pid = current->pid;
 		else if (unlikely(!strcmp(filename->name, ZYGOTE64_BIN)))
 			zygote64_pid = current->pid;
-	}
-
-	if (is_global_init(current->parent)) {
-		if (unlikely(!strncmp(filename->name, SEC_WLBTD_BIN_PREFIX, strlen(SEC_WLBTD_BIN_PREFIX))) ||
-				   unlikely(!strncmp(filename->name, SEC_WLAN_HAL_BIN_PREFIX, strlen(SEC_WLAN_HAL_BIN_PREFIX)))) {
-			current->flags |= PF_PERF_CRITICAL;
-			set_cpus_allowed_ptr(current, cpu_perf_mask);
-		}
 	}
 
 	/* execve succeeded */
