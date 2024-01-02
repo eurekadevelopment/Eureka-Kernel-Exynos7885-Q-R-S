eb42aff7420da4de2617d338f1786438d6768957
Soo Hwan Na <whiteshell2544@naver.com>
2024-01-02 05:56:03 +0000

    misc: modem_v1: Cut logs out

--- drivers/misc/modem_v1/modem_v1.h
+++ drivers/misc/modem_v1/modem_v1.h
@@ -595,12 +595,8 @@ struct modem_boot_spi {
 	printk_ratelimited(KERN_ERR LOG_TAG "%s: " pr_fmt(fmt), __func__, ##__VA_ARGS__)
 #define mif_err(fmt, ...) \
 	pr_err(LOG_TAG "%s: " pr_fmt(fmt), __func__, ##__VA_ARGS__)
-#define mif_debug(fmt, ...) \
-	pr_debug(LOG_TAG "%s: " pr_fmt(fmt), __func__, ##__VA_ARGS__)
-#define mif_info(fmt, ...) \
-	pr_info(LOG_TAG "%s: " pr_fmt(fmt), __func__, ##__VA_ARGS__)
-#define mif_trace(fmt, ...) \
-	printk(KERN_DEBUG "mif: %s: %d: called(%pF): " fmt, \
-		__func__, __LINE__, __builtin_return_address(0), ##__VA_ARGS__)
+#define mif_debug(fmt, ...)
+#define mif_info(fmt, ...)
+#define mif_trace(fmt, ...)
 
 #endif
