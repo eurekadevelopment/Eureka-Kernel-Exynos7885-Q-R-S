e55847bc1525daad56ba789a01fd34063efa03eb
kevios12 <kerschbaumer.kevin2@gmail.com>
2024-01-17 16:00:17 +0100

    Revert "mm: Disable readahead by default"
    This reverts commit b62f7d2062ca2602cc79088bc19dbda1bf450c2d.

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -2087,8 +2087,8 @@ int write_one_page(struct page *page, int wait);
 void task_dirty_inc(struct task_struct *tsk);
 
 /* readahead.c */
-#define VM_MAX_READAHEAD	0	/* kbytes */
-#define VM_MIN_READAHEAD	0	/* kbytes (includes current page) */
+#define VM_MAX_READAHEAD	128	/* kbytes */
+#define VM_MIN_READAHEAD	64	/* kbytes (includes current page) */
 extern int mmap_readaround_limit;
 
 int force_page_cache_readahead(struct address_space *mapping, struct file *filp,
