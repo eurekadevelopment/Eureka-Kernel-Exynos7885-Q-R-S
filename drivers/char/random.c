d0fd93019a0aa7a52353e2196369c2c91efb25c5
idkwhoiam322 <idkwhoiam322@raphielgang.org>
2024-02-09 13:38:55 +0100

    char/random: Mark random_read as __maybe_unused This silences the following warning: ../drivers/char/random.c:1841:1: warning: unused function 'random_read' [-Wunused-function] random_read(struct file *file, char __user *buf, size_t nbytes, loff_t *ppos) ^

--- drivers/char/random.c
+++ drivers/char/random.c
@@ -1446,7 +1446,7 @@ _random_read(int nonblock, char __user *buf, size_t nbytes)
 }
 
 #ifndef CONFIG_SRANDOM
-static ssize_t
+static ssize_t __maybe_unused
 random_read(struct file *file, char __user *buf, size_t nbytes, loff_t *ppos)
 {
 	return _random_read(file->f_flags & O_NONBLOCK, buf, nbytes);
