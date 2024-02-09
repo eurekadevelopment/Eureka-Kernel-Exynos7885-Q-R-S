8d220d15b57966396ebefd6a2443ca425f2fc883
Gian <gianpaoloestacio5@gmail.com>
2024-02-09 13:24:42 +0100

    fs: enable relatime

--- fs/namespace.c
+++ fs/namespace.c
@@ -3381,8 +3381,8 @@ long do_mount(const char *dev_name, const char __user *dir_name,
 		goto dput_out;
 
 	/* Default to relatime unless overriden */
-	//if (!(flags & MS_NOATIME))
-		//mnt_flags |= MNT_RELATIME;
+	if (!(flags & MS_NOATIME))
+		mnt_flags |= MNT_RELATIME;
 
 	/* Separate the per-mountpoint flags */
 	if (flags & MS_NOSUID)
